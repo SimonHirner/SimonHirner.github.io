3e80ee6fe70b772359249c9071b2c1d30ac378c8
Simon <simon.hirner99@gmail.com>
2022-02-28 00:05:45 +0100

    Customize template.

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import imageOverlay from "../img/earth.jpg";
+import obfuscate from "react-obfuscate";
 
 class Contact extends React.Component {
   render() {
     return (
       <section
         className="paralax-mf footer-paralax bg-image sect-mt4 route"
-        style={{ backgroundImage: "url(" + imageOverlay + ")" }}
+        style={{ backgroundColor: "#0078ff"}}
       >
         <div className="overlay-mf"></div>
         <div className="container">
@@ -15,88 +16,6 @@ class Contact extends React.Component {
               <div className="contact-mf">
                 <div id="contact" className="box-shadow-full">
                   <div className="row">
-                    <div className="col-md-6">
-                      <div className="title-box-2">
-                        <h5 className="title-left">Send A Message</h5>
-                      </div>
-                      <div>
-                        <form
-                          action="https://formspree.io/xdoeonlo"
-                          method="POST"
-                          className="contactForm"
-                        >
-                          <div id="sendmessage">
-                            Your message has been sent. Thank you!
-                          </div>
-                          <div id="errormessage"></div>
-                          <div className="row">
-                            <div className="col-md-12 mb-3">
-                              <div className="form-group">
-                                <input
-                                  type="text"
-                                  name="name"
-                                  className="form-control"
-                                  id="name"
-                                  placeholder="Your Name"
-                                  data-rule="minlen:4"
-                                  data-msg="Please enter at least 4 chars"
-                                />
-                                <div className="validation"></div>
-                              </div>
-                            </div>
-                            <div className="col-md-12 mb-3">
-                              <div className="form-group">
-                                <input
-                                  type="email"
-                                  className="form-control"
-                                  name="email"
-                                  id="email"
-                                  placeholder="Your Email"
-                                  data-rule="email"
-                                  data-msg="Please enter a valid email"
-                                />
-                                <div className="validation"></div>
-                              </div>
-                            </div>
-                            <div className="col-md-12 mb-3">
-                              <div className="form-group">
-                                <input
-                                  type="text"
-                                  className="form-control"
-                                  name="subject"
-                                  id="subject"
-                                  placeholder="Subject"
-                                  data-rule="minlen:4"
-                                  data-msg="Please enter at least 8 chars of subject"
-                                />
-                                <div className="validation"></div>
-                              </div>
-                            </div>
-                            <div className="col-md-12 mb-3">
-                              <div className="form-group">
-                                <textarea
-                                  className="form-control"
-                                  name="message"
-                                  rows="5"
-                                  data-rule="required"
-                                  data-msg="Please write something for us"
-                                  placeholder="Message"
-                                ></textarea>
-                                <div className="validation"></div>
-                              </div>
-                            </div>
-                            <div className="col-md-12">
-                              <button
-                                type="submit"
-                                className="button button-a button-big button-rouded"
-                              >
-                                Send Message
-                              </button>
-                            </div>
-                          </div>
-                        </form>
-                      </div>
-                    </div>
                     <div className="col-md-6">
                       <div className="title-box-2 pt-4 pt-md-0">
                         <h5 className="title-left">Get in Touch</h5>
@@ -172,6 +91,10 @@ class Contact extends React.Component {
       </section>
     );
   }
+}
+
+const doTheTrick = (str) => {
+  return str.replace(/[a-zA-Z]/g, function (c) { return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26); });
 }
 
 export default Contact;
--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -14,19 +14,20 @@ class Intro extends React.Component {
         <div className="intro-content display-table">
           <div className="table-cell">
             <div className="container">
-              <h1 className="intro-title mb-4">Hello, I am XYZ</h1>
+              <h1 className="intro-title mb-4">Simon Hirner</h1>
               <p className="intro-subtitle">
                 <span className="text-slider-items"></span>
                 <strong className="text-slider">
                   <Typed
                     strings={[
-                      "Front End Developer",
-                      "Back End Developer",
-                      "Software Engineer"
+                      "Software Engineering Student",
+                      "B.Sc. in Information Systems and Management",
+                      "BMW Fastlane Scholarship Holder",
+                      "Certified IT Management Assistant"
                     ]}
-                    typeSpeed={80}
+                    typeSpeed={70}
                     backDelay={1100}
-                    backSpeed={30}
+                    backSpeed={25}
                     loop
                   />
                 </strong>
